bd647f8a3d2413bf3bba58425c08866a349ec084
caferrerb <caferrerb@eam.edu.co>
2016-09-01 07:22:37 -0500

    arreglo

--- test/test/src/main/java/test2/PErsona.java
+++ test/test/src/main/java/test2/PErsona.java
@@ -5,8 +5,9 @@ public class PErsona {
 public PErsona(int a) {
 		a++;
 	}
-	public PErsona(int a, int b) {
+	public PErsona(int a, int b,int c) {
 		b=a;
+		c=a+b;
 	}
 	
 	public void metodo(int c){
