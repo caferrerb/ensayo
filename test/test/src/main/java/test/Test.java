21c804b06305ae4a105a0575574b82ec4a4a3d17
caferrerb <caferrerb@eam.edu.co>
2016-09-01 06:49:07 -0500

    cambios varios.

--- test/test/src/main/java/test/Test.java
+++ test/test/src/main/java/test/Test.java
@@ -1,5 +1,9 @@
 package test;
 
 public class Test {
+	
+public void superMetodo(){
+	System.err.println("jummmmm");
+}
 
 }
--- test/test/src/main/java/test/cosa.java
+++ test/test/src/main/java/test/cosa.java
@@ -2,4 +2,7 @@ package test;
 
 public class cosa {
 
+	public cosa() {
+		// TODO Auto-generated constructor stub
+	}
 }
